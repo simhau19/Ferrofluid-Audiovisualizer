254e35046a94c2902598650fc254dea0f50f8da1
Simen Haug <simehaug@stud.ntnu.no>
2023-03-20 22:49:08 +0100

    improved sampling time

--- src/main.cpp
+++ src/main.cpp
@@ -3,41 +3,36 @@
 #include "sample_periode.h"
 #include "fft.h"
 #include "ADCSampler.h"
+#include "adc.h"
 
-i2s_config_t adcI2SConfig = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
-    .sample_rate = 20000,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 4,
-    .dma_buf_len = 1024,
-    .use_apll = false,
-    .tx_desc_auto_clear = false,
-    .fixed_mclk = 0};
-
-ADCSampler adc(ADC_UNIT_1, ADC1_CHANNEL_0, adcI2SConfig);
+ADC2 adc(ADC2_CHANNEL_0);
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Setup");
-  adc.start();
+  adc.init();
 }
 
 void loop()
 {
-  const int sample_size = 1000;
-  int16_t samples[sample_size];
-
-  Serial.println("Start sampling");
+  // const int sample_size = 1000;
+  // Serial.println("Start sampling");
   
-  unsigned long last_micros = micros();
-  adc.read(samples, sample_size);
-  float sampling_time = ((float) micros() - last_micros) / (sample_size);
-
-  Serial.println(sampling_time);
+  // unsigned long last_micros = micros();
+  // for (int i = 0; i < sample_size; i++)
+  // {
+  //   adc.read();
+  // }
+  
+  // float sampling_time = ((float) micros() - last_micros)/sample_size;
+  // Serial.println(sampling_time);
+  Serial.print("CPU: ");
+  Serial.print(getCpuFrequencyMhz());
+  Serial.print("MHz\nXTAL: ");
+  Serial.print(getXtalFrequencyMhz());
+  Serial.print("MHz\nAPB: ");
+  Serial.println(getApbFrequency());
 
   delay(1000);
 }
